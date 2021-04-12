aa3122ce7a743f0ed1f0a2b645c3359e0115a34b
ramandeepsinghbhella <ramandeepsinghbhella@gmail.com>
2021-04-12 10:14:09 +0530

    Update main.py

--- 100daysofcode/higher-lower-start/main.py
+++ 100daysofcode/higher-lower-start/main.py
@@ -2,8 +2,9 @@ import random
 from game_data import data
 from art import logo
 from art import vs
+from replit import clear
 
-print(logo)
+
 
 def a(random_data_for_a):
   print(f"Compare A: {random_data_for_a['name']}, a {random_data_for_a['description']}, from {random_data_for_a['country']}")
@@ -12,32 +13,33 @@ def a(random_data_for_a):
 
 
 def b(random_data_for_b):
-  print(f"Against B: {random_data_for_b['name']}, a {random_data_for_b['description']}, from {random_data_for_b['country']}")
+  print(f"Against B: {random_data_for_b['name']}, a {random_data_for_b['description']}, from {random_data_for_b['country']}\n\n")
 
 game_continue = True
 score = 0
 
 while game_continue:
+  print(logo)
   random_data = random.choices(data, k=2)
   a(random_data[0])
   b(random_data[1])
 
   follower_count_for_a = random_data[0]['follower_count']
   follower_count_for_b = random_data[1]['follower_count']
-
-  users_choice = input("Who has more followers? Type A or B: ").lower()
-
+  print(f"Your score is {score}\n")
+  users_choice = input("Who has more followers? Type A or B: \n").lower()
   if users_choice == "a":
-    print(f"Your score is {score}")
+    
     if follower_count_for_a > follower_count_for_b:
       score+=1
+      clear()
     else:
       print(f"YOU LOSS' Your score is {score}")
       game_continue = False
   else:
-    print(f"Your score is {score}")
     if follower_count_for_b > follower_count_for_a:
       score+=1
+      clear()
     else:
-      print(f"YOU LOSS Your score is {score}")
+      print(f"YOU LOSS, Your score is {score}")
       game_continue = False
